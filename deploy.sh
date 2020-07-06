f948b82ef059db2af23afb17fff55ba5072413a2
KilFront <kaifawebb@gmail.com>
2020-07-06 10:17:39 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,7 @@
 echo 'git auto push start...'
 # msg=$1
 git add .
-# git commit -m "${$1}"
 git commit -m "$1"
-# echo 'git 提交注释:$1'
 # git pull origin master
 git push origin master
 echo 'git auto push end...'
