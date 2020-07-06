23b43ccf66894a9e5546ac2a7872e9eec2c639c7
KilFront <kaifawebb@gmail.com>
2020-07-06 10:39:10 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo 'git auto push start...'
 # msg=$1
 git add .
-git commit -m "${1:-'update'}"
+git commit -m "${1:-update}"
 # git pull origin master
 git push origin master
 echo 'git auto push end...'
